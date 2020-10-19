dcb351ea35c4b8ec4712ff511f232ddcf0a9cc65
‘LIANGYU <n795113@gmail.com>
2020-10-19 15:02:00 +0800

    Update: enable follow function in explore page

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,8 +1,11 @@
 @auth
 	@if(auth()->user()->isNot($user))
-		<form action="{{$user->username}}/follow" method="post">
+		<form action="{{ route('follow',$user->username) }}" method="post">
 			@csrf
-			<button class="bg-blue-400 rounded-full shadow p-2 text-white text-xs">	
+			<button class="
+				{{ auth()->user()->isFollow($user)?'bg-gray-400':'bg-blue-400' }}
+				rounded-full shadow p-2 text-white text-xs"
+			>	
 				{{auth()->user()->isFollow($user)? "Unfollow" : "Follow Me"}}
 			</button>
 		</form>
--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -1,15 +1,20 @@
 <x-app>
 	@foreach ($users as $user)
-		<a href="{{ $user->path() }}">
-			<div class="user flex items-center mb-4">
-				<div class="avatar">
-					<img src="{{ $user->getAvatar() }}" alt="" style="width: 50px">
-				</div>
+		<div class="flex items-center">
+			<a href="{{ $user->path() }}">
+				<div class="user flex items-center mb-4">
+					<div class="avatar">
+						<img src="{{ $user->getAvatar() }}" alt="" style="width: 50px">
+					</div>
 
-				<div class="name items-center">
-					{{ $user->name }}
+					<div class="name items-center">
+						{{ $user->name }}
+					</div>
 				</div>
-			</div>
-		</a>
+			</a>
+
+			<div class="ml-auto mr-0"></div>
+			<x-follow-button :user="$user"></x-follow-button>	
+		</div>
 	@endforeach
 </x-app>
