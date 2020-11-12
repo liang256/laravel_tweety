652e01b08652715123265afe67c9722c8e068441
LIANGYU <n795113@gmail.com>
2020-11-12 17:49:29 +0800

    fix: Add self_description for edit profile form

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -79,6 +79,24 @@
 			@enderror
 		</div> <!-- end email-input -->
 
+		<div class="self-description-input mb-6">
+			<label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+				Self description
+			</label>
+
+			<input class="border border-gray-400 p-2 w-full"
+				type="text" 
+				name="self_description" 
+				id="self-description"
+				required 
+				value="{{ $user->self_description }}"
+			>
+
+			@error('email')
+				<p class="text-red-200 text-xs mt-2">{{ $message }}</p>
+			@enderror
+		</div> <!-- end self-description-input -->
+
 		<div class="password-input mb-6">
 			<label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
 				password
